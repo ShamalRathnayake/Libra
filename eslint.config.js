dc3fefecb8978e922c704fbcfce28bb0c9a0cb28
ShamalRathnayake <shamalkavinda44@gmail.com>
2024-08-19 17:14:31 +0530

    initial setup

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettier,
+  {
+    ignores: ['node_modules/*'],
+  },
+  {
+    rules: {
+      'constructor-super': 'error',
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+    },
+  },
+];
