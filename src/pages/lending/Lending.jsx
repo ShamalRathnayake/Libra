b121827254a04371db25e3379b3b2769aa19d247
ShamalRathnayake <shamalkavinda44@gmail.com>
2024-09-07 02:44:27 +0530

    book select bugfix

--- src/pages/lending/Lending.jsx
+++ src/pages/lending/Lending.jsx
@@ -52,7 +52,7 @@ const Lending = () => {
   };
 
   const getBookTitle = (id) => {
-    const book = books?.find((book) => book.id === id);
+    const book = books?.find((book) => book.bookId === id);
     return book?.title;
   };
 
